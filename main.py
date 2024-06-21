d9719b0e2324f5615139cf83e84da23c562f4c20
Filip Wądołowski <84192488+fkwadolowski@users.noreply.github.com>
2024-06-21 03:22:41 +0200

    final part, instagram detects bot activity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 from selenium.webdriver.common.by import By
 import time
 
@@ -18,7 +20,8 @@ class InstaFollower:
         self.driver.get(URL)
         accept_cookies = self.driver.find_element(
             By.XPATH,
-            "/html/body/div[4]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[1]",
+            "/html/body/div[4]/div[1]/div/div[2]/div/div/div/div/div["
+            "2]/div/button[1]",
         )
         accept_cookies.click()
         time.sleep(1)
@@ -32,28 +35,49 @@ class InstaFollower:
             By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div'
         )
         login_button.click()
-        time.sleep(6)
+        time.sleep(5.9)
         save_info = self.driver.find_element(
             By.XPATH,
-            "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/section/div/button",
+            "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div["
+            "2]/section/main/div/div/div/section/div/button",
         )
         save_info.click()
-        time.sleep(3)
+        time.sleep(3.1)
         notif_button = self.driver.find_element(
             By.XPATH,
-            "/html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]",
+            "/html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div["
+            "2]/div/div/div[3]/button[2]",
         )
         notif_button.click()
 
     def find_followers(self):
         self.driver.get(f"{URL}/{SIMILAR_ACCOUNT}")
+        time.sleep(4.1)
         followers = self.driver.find_element(
             By.XPATH,
-            '//*[@id="mount_0_0_j+"]/div/div/div[2]/div/div/div[1]/div[2]/div/div[2]/section/main/div/header/section[3]/ul/li[2]/div',
+            "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div["
+            "2]/section/main/div/header/section["
+            "3]/ul/li[2]/div/a",
         )
+        followers.click()
+        time.sleep(3.7)
+        my_accout = self.driver.find_element(
+            By.XPATH,
+            "/html/body/div[6]/div[2]/div/div/div[1]/div/div["
+            "2]/div/div/div/div/div[2]/div/div/div[3]/div[1]/div/div[1]",
+        )
+        for _ in range(1):
+            time.sleep(2.5)
+            scroll_origin = ScrollOrigin.from_element(my_accout)
+            ActionChains(self.driver).scroll_from_origin(
+                scroll_origin, 0, 10000
+            ).perform()
 
     def follow(self):
-        pass
+        follow_button = self.driver.find_elements(By.CLASS_NAME, "_acas")
+        for button in follow_button:
+            button.click()
+            time.sleep(5.6)
 
 
 bot = InstaFollower()
